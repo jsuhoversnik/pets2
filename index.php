5d5398e73161056b0b50039392d2275e48e3daf0
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-01-18 13:20:25 -0500

    added index page

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//require autoload
+require_once('vendor/autoload.php');
+
+//create and instance of the Base class
+$f3 = Base::instance();
+//turn on fat free error reporting
+$f3->set('DEBUG',3);
+
+//define a default route
+$f3->route('GET /', function(){
+    echo '<h1>My Pets</h1>';
+    echo '<a href="order">Order a pet</a>';
+    //$view = new View;
+    //echo $view->render('views/home.html');
+});
+
+//run fat free
+$f3->run();
