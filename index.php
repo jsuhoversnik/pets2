2a14e316bb0fbb2df0c348a56c70bd7455865f6d
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-01-23 19:26:32 -0500

    Edited the results page so that it actually posted the thank you message properly.

--- index.php
+++ index.php
@@ -58,24 +58,24 @@ $f3->route('GET /order', function(){
     $view = new View;
     echo $view->render('views/form1.html');
 });
-$f3->route('POST /order2', function(){
+$f3->route('POST /order2', function($f3){
     //print_r($_POST);
     $_SESSION["animal"] = $_POST[animal];
+    $f3->set("animal", $_SESSION["animal"]);
     //print_r($_SESSION);
 
     $view = new View;
     echo $view->render('views/form2.html');
 });
 
-$f3->route('POST /results', function(){
+$f3->route('POST /results', function($f3){
     //print_r($_POST);
     $_SESSION["color"] = $_POST[color];
+    $f3->set("color", $_SESSION["color"]);
     //print_r($_SESSION);
 
     $template = new Template();
     echo $template->render('views/results.html');
-    echo "Thank you for ordering a " .
-        $_SESSION["color"] . " " . $_SESSION["animal"] . "!";
 });
 
 //run fat free
